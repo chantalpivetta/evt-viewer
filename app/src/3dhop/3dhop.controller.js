876fd54428c4e7946b6f9113f5b375ce9ff7d245
federicaspinelli <federicaspinelli@msn.com>
2020-04-29 09:47:50 +0200

    Attempt to resolve 3D model visualization

--- app/src/3dhop/3dhop.controller.js
+++ app/src/3dhop/3dhop.controller.js
@@ -1,6 +1,6 @@
-angular.module('evtviewer.3dhop')
+angular.module('evtviewer.3dhop', [])
 
 .controller('TreDHOPCtrl', function($log, $scope, $http) {
 	var vm = this;
-   var _console = $log.getInstance('tdhop');
+   var _console = $log.getInstance('3dhop');
 });
